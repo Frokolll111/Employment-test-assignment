3728ee745ed7c7dba027c7dca364a1240bc0b587
Frokolll111 <buther1433@gmail.com>
2024-01-13 07:51:00 +0700

    Тестовое задание для старта трудоустройства Сделал

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,37 @@
 package org.example;
 
+import com.gridnine.testing.Flight;
+import com.gridnine.testing.FlightBuilder;
+import com.gridnine.testing.Segment;
+
+import java.util.List;
+
+import static com.gridnine.testing.FlightBuilder.*;
+
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println(FlightBuilder.createFlights());
+        List<Flight> filteredFlights = filterFlights();
+        System.out.println("Фильтрованные вылеты до текущего момента времени: ");
+        for (Flight flight : filteredFlights) {
+            for (Segment segment : flight.getSegments()) {
+                System.out.println("Вылет: " + segment.getDepartureDate());
+            }
+        }
+        List<Flight> filteredFlights1 = filterFlights1();
+        System.out.println("Фильтрованные cегменты с датой прилёта раньше даты вылета:");
+        for (Flight flight : filteredFlights1) {
+            for (Segment segment : flight.getSegments()) {
+                System.out.println("Прилёт: " + segment.getArrivalDate());
+            }
+        }
+        List<Flight> filteredFlights2 = filterFlights2();
+        System.out.println("Перелеты, где общее время, проведённое на земле, превышает два часа: ");
+        for (Flight flight : filteredFlights2) {
+            for (Segment segment : flight.getSegments()) {
+                System.out.println("Вылет: " + segment.getDepartureDate() + " Прилёт: " + segment.getArrivalDate());
+            }
+        }
     }
 }
